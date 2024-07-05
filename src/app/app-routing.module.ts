ba94881b19aedef5baa8e013ab80c378b4959458
Shwetasatpute93 <shwetasatpute93@gmail.com>
2024-07-05 12:46:17 +0530

    demo all code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  path: '',
+  component: LandingComponent
+ },
+ {
+   path: 'home',
+   component: EnquiryComponent
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
